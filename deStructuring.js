1e98ade2a0a84e423ed2e95709e68a11cb9e95ae
SUBHIN-TM <subhintm2018@gmail.com>
2023-11-22 20:16:18 +0530

    first

---
+++ deStructuring.js
@@ -0,0 +1,40 @@
+const array=[1,2,3]
+const {0:first,1:second,2:third}=array
+const result ={first,second,third}
+console.log(result);
+
+
+
+
+
+//excerse 2
+const input = {name:'subhin',age:30}
+const {age,name} = input   //variable name should be equal to variable name  
+console.log(name);
+console.log(age);
+
+
+
+//exrse 3
+const inp = [1,2,3,4,5]
+const [one,two,three,four] = inp
+console.log(one,two,three,four);
+
+
+
+
+// excrs 4
+const midhu = [1,2,3]
+const  {0:x,1:y,2:z} =midhu
+console.log(z);
+
+
+
+
+// exrce 5 
+let g = {fname:'midhumon' ,id:121,place:'kerala'}
+g.fname=123
+console.log(g.fname);
+
+
+
