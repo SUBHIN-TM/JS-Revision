990dacf674c3f462aa60d0a85cf3d744b196772a
SUBHIN-TM <subhintm2018@gmail.com>
2023-11-22 20:19:42 +0530

    second

--- promise.js
+++ promise.js
@@ -23,35 +23,35 @@ var myFunction= (x) => {
 
 // METHOD 1 
 
-//  myFunction('subhin')
-//  .then((k) => {  //promise resolve state
-//    try{
-//     console.log(k);
-//    }
-//    catch(error){//insiode this block any error occur then this catch will took
-//     console.error(`error due to the inside then block ${error}`);
-//    }
+ myFunction('subhin')
+ .then((k) => {  //promise resolve state
+   try{
+    console.log(k);
+   }
+   catch(error){//insiode this block any error occur then this catch will took
+    console.error(`error due to the inside then block ${error}`);
+   }
    
-//  })
-// .catch((error) => {  //HERE THIS CATCH WILL FIND THE PROMISE CHAIN ERRORS,promise reject content
-//     console.error(`error dueto promise function ${error}`);
-// })
+ })
+.catch((error) => {  //HERE THIS CATCH WILL FIND THE PROMISE CHAIN ERRORS,promise reject content
+    console.error(`error dueto promise function ${error}`);
+})
 
 
 
 
 // OR   METHOD 2
 
-// (async () => {
-//     try{
-//         console.log('writing inside aync function');
-//         let result = await myFunction('subhin')
-//         console.log(result);
-//     }
-//     catch(error){
-//         console.error('due to',error);
-//     }
-// }) ()
+(async () => {
+    try{
+        console.log('writing inside aync function');
+        let result = await myFunction('subhin')
+        console.log(result);
+    }
+    catch(error){
+        console.error('due to',error);
+    }
+}) ()
 
 
 
@@ -78,24 +78,21 @@ calling()
 
 
 
-
-
-
 // ARAFFU Promise
 
-// const p = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         resolve("ok");
-//     }, 5000);
-// })
+const p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("ok");
+    }, 5000);
+})
 
 
 
 
-// async function a() {
-//     console.log("hii");
-//     const c = await p;
-//     console.log("gwfd");
-//     console.log(c);
-// }
-// a();
+async function a() {
+    console.log("hii");
+    const c = await p;
+    console.log("gwfd");
+    console.log(c);
+}
+a();
